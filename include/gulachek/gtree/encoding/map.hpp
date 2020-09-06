3221a7a18cf6549973bf79d0351d311244477af8
Nicholas Gulachek <gulac004@umn.edu>
2020-09-06 02:11:16 -0500

    Make map use pair encoding

--- include/gulachek/gtree/encoding/map.hpp
+++ include/gulachek/gtree/encoding/map.hpp
@@ -1,6 +1,9 @@
 #ifndef GULACHEK_GTREE_ENCODING_MAP_HPP
 #define GULACHEK_GTREE_ENCODING_MAP_HPP
 
+#include "gulachek/gtree/encoding/encoding.hpp"
+#include "gulachek/gtree/encoding/pair.hpp"
+
 #include <map>
 
 namespace gulachek::gtree
@@ -8,76 +11,14 @@ namespace gulachek::gtree
 	template <typename K, typename V, typename C, typename A>
 	struct uses_value<std::map<K,V,C,A>> : std::false_type {};
 
-	template <typename MutableTree, typename K, typename V>
-	void __encode_kv(const K &k, const V &v, MutableTree &tree)
-	{
-		if constexpr (
-				(uses_children<K>::value || uses_value<V>::value) &&
-				(uses_children<V>::value || uses_value<K>::value)
-				)
-		{
-			// { K V }
-			if constexpr (uses_children<K>::value)
-			{
-				tree.child_count(2);
-				encode(k, tree.child(0));
-				encode(v, tree.child(1));
-			}
-			// { K | V }
-			else
-			{
-				tree.child_count(1);
-				encode(k, tree);
-				encode(v, tree.child(0));
-			}
-		}
-		// K = V (same node)
-		else
-		{
-			encode(k, tree);
-			encode(v, tree);
-		}
-	}
-
-
 	template <typename MutableTree, typename K, typename V, typename C, typename A>
 	void encode(const std::map<K,V,C,A> &val, MutableTree &tree)
 	{
 		tree.child_count(val.size());
 
 		std::size_t i = 0;
-		for (const auto &kv : val)
-			__encode_kv<MutableTree, K, V>(
-					kv.first, kv.second, tree.child(i++));
-	}
-
-	template <typename Tree, typename K, typename V>
-	void __decode_kv(const Tree &tree, K &k, V &v)
-	{
-		if constexpr (
-				(uses_children<K>::value || uses_value<V>::value) &&
-				(uses_children<V>::value || uses_value<K>::value)
-				)
-		{
-			// { K V }
-			if constexpr (uses_children<K>::value)
-			{
-				decode(tree.child(0), k);
-				decode(tree.child(1), v);
-			}
-			// { K | V }
-			else
-			{
-				decode(tree, k);
-				decode(tree.child(0), v);
-			}
-		}
-		// K = V (same node)
-		else
-		{
-			decode(tree, k);
-			decode(tree, v);
-		}
+		for (const std::pair<K,V> &kv : val)
+			encode(kv, tree.child(i++));
 	}
 
 	template <typename Tree, typename K, typename V, typename C, typename A>
@@ -87,9 +28,9 @@ namespace gulachek::gtree
 
 		for (std::size_t i = 0; i < tree.child_count(); i++)
 		{
-			K k; V v;
-			__decode_kv<Tree, K, V>(tree.child(i), k, v);
-			val.emplace(std::move(k), std::move(v));
+			std::pair<K, V> kv;
+			decode(tree.child(i), kv);
+			val.emplace(std::move(kv));
 		}
 	}
 }
