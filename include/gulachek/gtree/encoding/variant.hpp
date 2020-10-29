4867be4ee87634f703ecaf9812fd6abc8967820d
Nicholas Gulachek <gulac004@umn.edu>
2020-10-28 23:24:48 -0500

    Make variant encoding more generic

--- include/gulachek/gtree/encoding/variant.hpp
+++ include/gulachek/gtree/encoding/variant.hpp
@@ -1,40 +1,100 @@
 #ifndef GULACHEK_GTREE_ENCODING_VARIANT_HPP
 #define GULACHEK_GTREE_ENCODING_VARIANT_HPP
 
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/at.hpp>
+#include <boost/mpl/size.hpp>
+#include <boost/utility/value_init.hpp>
 #include <variant>
 #include <type_traits>
 
 namespace gulachek::gtree
 {
+	// Specialize this with:
+	// 	MPL sequence of variant alternative types
+	// 	typedef ... sequence;
+	//
+	// 	Runtime index of an instance of the variant type
+	// 	std::size_t index(const Variant &);
+	template <typename Variant>
+	struct variant_encoding
+	{};
+
+	// Specialization for std::variant
+	template <typename ...Ts>
+	struct variant_encoding<std::variant<Ts...>>
+	{
+		typedef typename boost::mpl::vector<Ts...>::type sequence;
+
+		static std::size_t index(const std::variant<Ts...> &v)
+		{ return v.index(); }
+
+		template <typename T>
+		static const T& get(const std::variant<Ts...> &v)
+		{ return std::get<T>(v); }
+	};
+
 	template <
-		std::size_t n,
-		typename Tree,
-		typename Variant,
-		std::enable_if_t<n == std::variant_size_v<Variant>, int> = 0
-						 >
-	void __encode_variant(
-			const Variant &val,
-			Tree &tr
-			)
+		std::size_t I,
+		typename Sequence,
+		typename Function,
+		std::enable_if_t<
+			I >= boost::mpl::size<Sequence>::value,
+			void*> = nullptr
+			>
+	void __run_at_elem(std::size_t i, Function &&f)
 	{
 		// don't worry, be happy
 	}
 
 	template <
-		std::size_t n,
-		typename Tree,
-		typename Variant,
-		std::enable_if_t<n < std::variant_size_v<Variant>, int> = 0
-						 >
-	void __encode_variant(
-			const Variant &val,
-			Tree &tr
-			)
+		std::size_t I,
+		typename Sequence,
+		typename Function,
+		std::enable_if_t<
+			I < boost::mpl::size<Sequence>::value,
+			void*> = nullptr
+			>
+	void __run_at_elem(std::size_t i, Function &&f)
 	{
-		if (n == val.index())
+		if (i == I)
 		{
-			using T = typename std::variant_alternative<n, Variant>::type;
-			auto &ref = std::get<T>(val);
+			boost::value_initialized<
+				typename boost::mpl::at_c<Sequence, I>::type
+				> v;
+
+			std::invoke(
+					std::forward<Function>(f),
+					boost::get(v)
+					);
+		}
+		else
+		{
+			__run_at_elem<I+1, Sequence, Function>(
+					i,
+					std::forward<Function>(f)
+					);
+		}
+	}
+
+	template <typename Sequence, typename Function>
+	void __run_at_elem(std::size_t i, Function &&f)
+	{
+		__run_at_elem<0, Sequence, Function>
+			(i, std::forward<Function>(f));
+	}
+
+	template <typename Variant, typename Tree>
+	struct __encode_functor
+	{
+		const Variant &val;
+		Tree &tr;
+
+		template <typename T>
+		void operator() (T)
+		{
+			auto &ref = variant_encoding<Variant>::
+				template get<T>(val);
 
 			if constexpr (uses_value<T>::value)
 			{
@@ -46,82 +106,66 @@ namespace gulachek::gtree
 				encode(ref, tr);
 			}
 		}
-
-		__encode_variant<n+1>(val, tr);
-	}
+	};
 
 	// Variant has type index as value and data as first child
-	template <typename Tree, typename... Types>
+	template <
+		typename Tree,
+		typename Variant,
+		typename variant_encoding<Variant>::sequence* = nullptr
+			 >
 	void encode(
-			const std::variant<Types...> &val,
-			Tree &tr
+			const Variant &val,
+			Tree &tree
 			)
 	{
-		// we need to write the index and reserve a child
-		encode(val.index(), tr);
+		typedef typename variant_encoding<Variant>::sequence seq;
+		auto index = variant_encoding<Variant>::index(val);
 
-		__encode_variant<0>(val, tr);
+		encode(index, tree);
+
+		__encode_functor<Variant, Tree> ftor{val, tree};
+		__run_at_elem<seq>(index, ftor);
 	}
 
-	template <
-		std::size_t n,
-		typename Tree,
-		typename Variant,
-		std::enable_if_t<n == std::variant_size_v<Variant>, int> = 0
-						 >
-	void __decode_variant(
-			const Tree &tr,
-			Variant &val,
-			std::size_t index
-			)
+	template <typename Variant, typename Tree>
+	struct __decode_functor
 	{
-		// don't care if not following conventions
-		// just return
-	}
+		const Tree &tr;
+		Variant &val;
 
-
-	// This could eventually be more efficient
-	template <
-		std::size_t n,
-		typename Tree,
-		typename Variant,
-		std::enable_if_t<n < std::variant_size_v<Variant>, int> = 0
-						 >
-	void __decode_variant(
-			const Tree &tr,
-			Variant &val,
-			std::size_t index
-			)
-	{
-		if (index == n)
+		template <typename T>
+		void operator() (T)
 		{
-			std::variant_alternative_t<n, Variant> elem;
+			T elem;
 
 			// No need for indirection if no value is in the way
-			if constexpr (uses_value<decltype(elem)>::value)
+			if constexpr (uses_value<T>::value)
 				decode(tr.child(0), elem);
 			else
 				decode(tr, elem);
 
 			val = std::move(elem);
 		}
+	};
 
-		__decode_variant<n+1, Tree, Variant>(tr, val, index);
-	}
-
-	template <typename Tree, typename... Types>
+	template <
+		typename Tree,
+		typename Variant,
+		typename variant_encoding<Variant>::sequence* = nullptr
+		>
 	void decode(
-			const Tree &tr,
-			std::variant<Types...> &val
+			const Tree &tree,
+			Variant &val
 			)
 	{
-		// read the index
+		typedef typename variant_encoding<Variant>::sequence seq;
 		std::size_t index;
-		decode(tr, index);
+		decode(tree, index);
 
-		__decode_variant<0>(tr, val, index);
+		__decode_functor<Variant, Tree> ftor{tree, val};
+		__run_at_elem<seq>(index, ftor);
 	}
-
 }
 
 #endif
