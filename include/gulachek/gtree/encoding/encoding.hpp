eb3386855c467d1f7f7ebc4344f836e24048ec8f
Nicholas Gulachek <gulac004@umn.edu>
2021-07-21 22:32:22 -0500

    More constraints

--- include/gulachek/gtree/encoding/encoding.hpp
+++ include/gulachek/gtree/encoding/encoding.hpp
@@ -18,29 +18,10 @@ namespace gulachek::gtree
 	// encoding concept
 	template <typename T, typename Enable = void>
 	struct encoding
-	{
-		// using type = T;
-
-		// static constexpr bool uses_value;
-		// static constexpr bool uses_children;
-
-		// template <typename T, typename MutableTree>
-		// static error encode(T &&val, MutableTree &tr);
-
-		// template <typename Tree, typename T>
-		// static error decode(Tree &&tr, T &val);
-	};
+	{};
 
 	template <typename T>
 	struct encoding_t : encoding<std::decay_t<T>> {};
-
-	// Can the type be decoded? Not intended for specialization.
-	template <typename T>
-	struct is_sertreealizable : std::conjunction<
-										 std::is_default_constructible<T>,
-										 std::is_move_constructible<T>,
-										 std::is_move_assignable<T>
-															> {};
 
 	template <bool Anything>
 	constexpr bool constexpr_bool = Anything;
@@ -69,40 +50,30 @@ namespace gulachek::gtree
 	concept Sertreealizable = Sertreealizable_<std::decay_t<T>>;
 
 	// Does the type's encoding ever have a value?
-	template <typename T>
+	template <Sertreealizable T>
 	struct uses_value
 	{
 		static constexpr bool value = encoding_t<T>::uses_value;
 	};
 
-	template <typename T>
+	template <Sertreealizable T>
 	struct is_pure_container
 	{
 		static constexpr bool value = !uses_value<T>::value;
 	};
 
 	// Does the type's encoding ever have children?
-	template <typename T>
+	template <Sertreealizable T>
 	struct uses_children
 	{
 		static constexpr bool value = encoding_t<T>::uses_children;
 	};
 
-	template <typename T>
+	template <Sertreealizable T>
 	struct is_pure_value
 	{
 		static constexpr bool value = !uses_children<T>::value;
 	};
-
-	// Encode object into a tree
-	// Caller is responsible for making copy if value must be constant
-	// template <typename T, typename MutableTree>
-	// error encode(T &&val, MutableTree &tree);
-
-	// Decode object from a tree
-	// Caller is responsible for making copy if value must be constant
-	// template <typename Tree, typename T>
-	// error decode(Tree &&t, T &val);
 
 	template <Sertreealizable T, MutableTree Tree>
 	error encode(const T &val, Tree &tr)
