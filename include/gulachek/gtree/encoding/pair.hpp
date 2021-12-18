1fd51d9c3240145a8c0bbeacb433f471525fba40
Nicholas Gulachek <gulac004@umn.edu>
2021-12-18 11:02:47 -0600

    Lose optimization on std::pair encoding

--- include/gulachek/gtree/encoding/pair.hpp
+++ include/gulachek/gtree/encoding/pair.hpp
@@ -11,104 +11,39 @@ namespace gulachek::gtree
 		using type = std::pair<K,V>;
 
 		static constexpr bool uses_children = true;
-		static constexpr bool uses_value = 
-				is_pure_value<K>::value || is_pure_value<V>::value;
+		static constexpr bool uses_value = false;
 
 		template <typename Tree>
 		static error decode(Tree &&tree, type &pair)
 		{
-			if constexpr (is_pure_value<K>::value)
-			{
-				if (auto err = gtree::decode(std::forward<Tree>(tree), pair.first))
-					return err;
+			if (tree.child_count() < 2)
+				return {"expected 2 children for pair"};
 
-				if constexpr (is_pure_container<V>::value)
-				{
-					if (auto err = gtree::decode(std::forward<Tree>(tree), pair.second))
-						return err;
-				}
-				else
-				{
-					if (auto err = gtree::decode(std::forward<Tree>(tree).child(0), pair.second))
-						return err;
-				}
-			}
-			else if constexpr (is_pure_value<V>::value)
+			if (auto err =
+					gtree::decode(std::forward<Tree>(tree).child(0),
+						pair.first
+						)
+				 )
 			{
-				if (auto err = gtree::decode(std::forward<Tree>(tree), pair.second))
-					return err;
-
-				if constexpr (is_pure_container<K>::value)
-				{
-					if (auto err = gtree::decode(std::forward<Tree>(tree), pair.first))
-						return err;
-				}
-				else
-				{
-					if (auto err = gtree::decode(std::forward<Tree>(tree).child(0), pair.first))
-						return err;
-				}
-			}
-			else
-			{
-				if (auto err = gtree::decode(std::forward<Tree>(tree).child(0), pair.first))
-					return err;
-
-				if (auto err = gtree::decode(std::forward<Tree>(tree).child(1), pair.second))
-					return err;
+				return err;
 			}
 
-			return {};
+			return gtree::decode(std::forward<Tree>(tree).child(1),
+					pair.second);
 		}
 
 		template <typename Pair, typename MutableTree>
 		static error encode(Pair &&pair, MutableTree &tree)
 		{
-			if constexpr (is_pure_value<K>::value)
+			tree.child_count(2);
+			if (auto err = gtree::encode(std::forward<Pair>(pair).first,
+						tree.child(0)))
 			{
-				if (auto err = gtree::encode(std::forward<Pair>(pair).first, tree))
-					return err;
-
-				if constexpr (is_pure_container<V>::value)
-				{
-					if (auto err = gtree::encode(std::forward<Pair>(pair).second, tree))
-						return err;
-				}
-				else
-				{
-					tree.child_count(1);
-					if (auto err = gtree::encode(std::forward<Pair>(pair).second, tree.child(0)))
-						return err;
-				}
-			}
-			else if constexpr (is_pure_value<V>::value)
-			{
-				if (auto err = gtree::encode(std::forward<Pair>(pair).second, tree))
-					return err;
-
-				if constexpr (is_pure_container<K>::value)
-				{
-					if (auto err = gtree::encode(std::forward<Pair>(pair).first, tree))
-						return err;
-				}
-				else
-				{
-					tree.child_count(1);
-					if (auto err = gtree::encode(std::forward<Pair>(pair).first, tree.child(0)))
-						return err;
-				}
-			}
-			else
-			{
-				tree.child_count(2);
-				if (auto err = gtree::encode(std::forward<Pair>(pair).first, tree.child(0)))
-					return err;
-
-				if (auto err = gtree::encode(std::forward<Pair>(pair).second, tree.child(1)))
-					return err;
+				return err;
 			}
 
-			return {};
+			return gtree::encode(std::forward<Pair>(pair).second,
+					tree.child(1));
 		}
 	};
 }
--- test/pair_encoding_test.cpp
+++ test/pair_encoding_test.cpp
@@ -19,42 +19,19 @@ namespace bd = boost::unit_test::data;
 namespace gt = gulachek::gtree;
 
 using uint = unsigned int;
-using vec = std::vector<uint>;
 using tree = gt::mutable_tree;
-
-using pval_pval = std::pair<uint, uint>;
-using pval_pcon = std::pair<uint, vec>;
-using pval_gval = std::pair<uint, pval_pval>;
-using pcon_pval = std::pair<vec, uint>;
-using gval_pval = std::pair<pval_pval, uint>;
-using pcon_pcon = std::pair<vec, vec>;
-
-vec v123 = {1, 2, 3};
-vec v456 = {4, 5, 6};
-
-BOOST_AUTO_TEST_SUITE(PValPVal)
-
-BOOST_AUTO_TEST_CASE(UsesValue)
-{
-	BOOST_TEST(gt::uses_value<pval_pval>::value);
-}
-
-BOOST_AUTO_TEST_CASE(UsesChildren)
-{
-	BOOST_TEST(gt::uses_children<pval_pval>::value);
-}
 
 BOOST_AUTO_TEST_CASE(Encode)
 {
-	pval_pval pair = {1, 2};
+	std::pair<uint, uint> pair = {1, 2};
 
 	tree tr;
 	BOOST_TEST(!gt::encode(pair, tr));
 
-	BOOST_REQUIRE(tr.child_count() == 1);
+	BOOST_REQUIRE(tr.child_count() == 2);
 	uint first = 0, second = 0;
-	gt::decode(tr, first);
-	gt::decode(tr.child(0), second);
+	gt::decode(tr.child(0), first);
+	gt::decode(tr.child(1), second);
 
 	BOOST_TEST(first == 1);
 	BOOST_TEST(second == 2);
@@ -62,240 +39,13 @@ BOOST_AUTO_TEST_CASE(Encode)
 
 BOOST_AUTO_TEST_CASE(Decode)
 {
-	pval_pval truth = {1, 2};
+	std::pair<uint, uint> truth = {1, 2};
 	tree tr;
 	gt::encode(truth, tr);
 
-	pval_pval pair = {0,0};
+	std::pair<uint, uint> pair = {0,0};
 	BOOST_TEST(!gt::decode(tr, pair));
 
 	BOOST_TEST(pair.first == 1);
 	BOOST_TEST(pair.second == 2);
 }
-
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(PValPCon)
-
-BOOST_AUTO_TEST_CASE(UsesValue)
-{
-	BOOST_TEST(gt::uses_value<pval_pcon>::value);
-}
-
-BOOST_AUTO_TEST_CASE(UsesChildren)
-{
-	BOOST_TEST(gt::uses_children<pval_pcon>::value);
-}
-
-BOOST_AUTO_TEST_CASE(Encode)
-{
-	pval_pcon pair = {1, v123};
-
-	tree tr;
-	BOOST_TEST(!gt::encode(pair, tr));
-
-	uint n = 0;
-	vec v;
-	gt::decode(tr, n);
-	gt::decode(tr, v);
-
-	BOOST_TEST(n == 1);
-	BOOST_TEST(v == v123, tt::per_element());
-}
-
-BOOST_AUTO_TEST_CASE(Decode)
-{
-	pval_pcon truth = {1, v123};
-	tree tr;
-	gt::encode(truth, tr);
-
-	pval_pcon pair = {0,{}};
-	BOOST_TEST(!gt::decode(tr, pair));
-
-	BOOST_TEST(pair.first == 1);
-	BOOST_TEST(pair.second == v123, tt::per_element());
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(PValGVal)
-
-BOOST_AUTO_TEST_CASE(UsesValue)
-{
-	BOOST_TEST(gt::uses_value<pval_gval>::value);
-}
-
-BOOST_AUTO_TEST_CASE(UsesChildren)
-{
-	BOOST_TEST(gt::uses_children<pval_gval>::value);
-}
-
-BOOST_AUTO_TEST_CASE(Encode)
-{
-	pval_gval pair = {1, {2, 3}};
-
-	tree tr;
-	BOOST_TEST(!gt::encode(pair, tr));
-
-	BOOST_REQUIRE(tr.child_count() == 1);
-
-	uint a = 0;
-	pval_pval bc = {0,0};
-
-	gt::decode(tr, a);
-	gt::decode(tr.child(0), bc);
-
-	BOOST_TEST(a == 1);
-	BOOST_TEST(bc.first == 2);
-	BOOST_TEST(bc.second == 3);
-}
-
-BOOST_AUTO_TEST_CASE(Decode)
-{
-	pval_gval truth = {1, {2,3}};
-	tree tr;
-	gt::encode(truth, tr);
-
-	pval_gval pair = {0,{0,0}};
-	BOOST_TEST(!gt::decode(tr, pair));
-
-	BOOST_TEST(pair.first == 1);
-	BOOST_TEST(pair.second.first == 2);
-	BOOST_TEST(pair.second.second == 3);
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(PConPVal)
-
-BOOST_AUTO_TEST_CASE(UsesValue)
-{
-	BOOST_TEST(gt::uses_value<pcon_pval>::value);
-}
-
-BOOST_AUTO_TEST_CASE(UsesChildren)
-{
-	BOOST_TEST(gt::uses_children<pcon_pval>::value);
-}
-
-BOOST_AUTO_TEST_CASE(Encode)
-{
-	pcon_pval pair = {v123, 4};
-
-	tree tr;
-	BOOST_TEST(!gt::encode(pair, tr));
-
-	uint n = 0;
-	vec v;
-	gt::decode(tr, n);
-	gt::decode(tr, v);
-
-	BOOST_TEST(v == v123, tt::per_element());
-	BOOST_TEST(n == 4);
-}
-
-BOOST_AUTO_TEST_CASE(Decode)
-{
-	pcon_pval truth = {v123,4};
-	tree tr;
-	gt::encode(truth, tr);
-
-	pcon_pval pair = {{},0};
-	BOOST_TEST(!gt::decode(tr, pair));
-
-	BOOST_TEST(pair.first == v123, tt::per_element());
-	BOOST_TEST(pair.second == 4);
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(GValPVal)
-
-BOOST_AUTO_TEST_CASE(UsesValue)
-{
-	BOOST_TEST(gt::uses_value<gval_pval>::value);
-}
-
-BOOST_AUTO_TEST_CASE(UsesChildren)
-{
-	BOOST_TEST(gt::uses_children<gval_pval>::value);
-}
-
-BOOST_AUTO_TEST_CASE(Encode)
-{
-	gval_pval pair = {{1, 2}, 3};
-
-	tree tr;
-	BOOST_TEST(!gt::encode(pair, tr));
-
-	BOOST_REQUIRE(tr.child_count() == 1);
-
-	pval_pval ab = {0,0};
-	uint c = 0;
-
-	gt::decode(tr, c);
-	gt::decode(tr.child(0), ab);
-
-	BOOST_TEST(ab.first == 1);
-	BOOST_TEST(ab.second == 2);
-	BOOST_TEST(c == 3);
-}
-
-BOOST_AUTO_TEST_CASE(Decode)
-{
-	gval_pval truth = {{1,2},3};
-	tree tr;
-	gt::encode(truth, tr);
-
-	gval_pval pair = {{0,0},0};
-	BOOST_TEST(!gt::decode(tr, pair));
-
-	BOOST_TEST(pair.first.first == 1);
-	BOOST_TEST(pair.first.second == 2);
-	BOOST_TEST(pair.second == 3);
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(PConPCon)
-
-BOOST_AUTO_TEST_CASE(NoValue)
-{
-	BOOST_TEST(!gt::uses_value<pcon_pcon>::value);
-}
-
-BOOST_AUTO_TEST_CASE(UsesChildren)
-{
-	BOOST_TEST(gt::uses_children<pcon_pcon>::value);
-}
-
-BOOST_AUTO_TEST_CASE(Encode)
-{
-	pcon_pcon pair = {v123, v456};
-
-	tree tr;
-	BOOST_TEST(!gt::encode(pair, tr));
-
-	BOOST_REQUIRE(tr.child_count() == 2);
-	vec u, v;
-	gt::decode(tr.child(0), u);
-	gt::decode(tr.child(1), v);
-
-	BOOST_TEST(u == v123, tt::per_element());
-	BOOST_TEST(v == v456, tt::per_element());
-}
-
-BOOST_AUTO_TEST_CASE(Decode)
-{
-	pcon_pcon truth = {v123,v456};
-	tree tr;
-	gt::encode(truth, tr);
-
-	pcon_pcon pair = {{},{}};
-	BOOST_TEST(!gt::decode(tr, pair));
-
-	BOOST_TEST(pair.first == v123, tt::per_element());
-	BOOST_TEST(pair.second == v456, tt::per_element());
-}
-
-BOOST_AUTO_TEST_SUITE_END()
