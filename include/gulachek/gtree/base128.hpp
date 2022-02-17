57bab55a62cb56ef7a550b194a9fc8d081385590
Nicholas Gulachek <gulac004@umn.edu>
2022-02-17 07:25:58 -0600

    Refactor base128 into source file

--- include/gulachek/gtree/base128.hpp
+++ include/gulachek/gtree/base128.hpp
@@ -15,51 +15,7 @@ namespace gulachek::gtree
 		overflow
 	};
 
-	cause read_base128(std::istream &is, std::size_t *n)
-	{
-		*n = 0;
-		std::size_t power = 1;
-		std::size_t b;
-		std::size_t nbits = 7;
-
-		auto c = is.get();
-		auto eof = std::istream::traits_type::eof();
-
-		if (c == eof)
-		{
-			if (is.eof())
-				return cause::eof();
-			else
-				return {read_base128_error::bad_stream, "bad stream"};
-		}
-
-		b = (std::size_t) c;
-		while (b & 0x80)
-		{
-			*n += (power * (b & 0x7f));
-			power *= 128;
-			nbits += 7;
-
-			c = is.get();
-			if (c == eof)
-			{
-				return
-				{
-					read_base128_error::incomplete,
-					"incomplete base 127 integer"
-				};
-			}
-
-			b = (std::size_t) c;
-
-			if ((nbits > 8*sizeof(std::size_t)) && b > 1)
-				return {read_base128_error::overflow, "overflow"};
-
-		}
-
-		*n += (power * b);
-		return {};
-	}
+	cause read_base128(std::istream &is, std::size_t *n);
 }
 
 #endif
---
+++ src/base128.cpp
@@ -0,0 +1,50 @@
+#include "gulachek/gtree/base128.hpp"
+
+namespace gulachek::gtree
+{
+	cause read_base128(std::istream &is, std::size_t *n)
+	{
+		*n = 0;
+		std::size_t power = 1;
+		std::size_t b;
+		std::size_t nbits = 7;
+
+		auto c = is.get();
+		auto eof = std::istream::traits_type::eof();
+
+		if (c == eof)
+		{
+			if (is.eof())
+				return cause::eof();
+			else
+				return {read_base128_error::bad_stream, "bad stream"};
+		}
+
+		b = (std::size_t) c;
+		while (b & 0x80)
+		{
+			*n += (power * (b & 0x7f));
+			power *= 128;
+			nbits += 7;
+
+			c = is.get();
+			if (c == eof)
+			{
+				return
+				{
+					read_base128_error::incomplete,
+					"incomplete base 127 integer"
+				};
+			}
+
+			b = (std::size_t) c;
+
+			if ((nbits > 8*sizeof(std::size_t)) && b > 1)
+				return {read_base128_error::overflow, "overflow"};
+
+		}
+
+		*n += (power * b);
+		return {};
+	}
+}
