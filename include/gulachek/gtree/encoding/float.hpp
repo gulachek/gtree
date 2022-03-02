bd4e54b46158ef3b9dd862e4862010871954dbc0
Nicholas Gulachek <gulac004@umn.edu>
2022-03-01 22:47:31 -0600

    Add float support

--- include/gulachek/gtree/encoding/float.hpp
+++ include/gulachek/gtree/encoding/float.hpp
@@ -1,7 +1,8 @@
 #ifndef GULACHEK_GTREE_ENCODING_FLOAT_HPP
 #define GULACHEK_GTREE_ENCODING_FLOAT_HPP
 
-#include "gulachek/gtree/encoding/encoding.hpp"
+#include "gulachek/gtree/encoding.hpp"
+#include "gulachek/gtree/decoding.hpp"
 
 #include <type_traits>
 #include <cmath>
@@ -9,14 +10,86 @@
 namespace gulachek::gtree
 {
 	template <>
-	struct encoding<float, void>
+	struct decoding<float>
 	{
-		using type = float;
+		float *pf;
 
-		template <typename MutableTree>
-		static error encode(float f, MutableTree &tree)
+		cause decode(treeder &r)
+		{
+			float f = 0.0f;
+
+			// Naive implementation... optimize by compiler later
+			auto value = r.value();
+			if (value.size() != 4)
+			{
+				cause err;
+				err << "Expected float to have 4 bytes. Tree had " << value.size();
+				return err;
+			}
+
+			auto b = value.data();
+
+			float sign = (0x80 & b[3]) ? -1.0f : 1.0f;
+			int exp =
+				((0x7f & b[3]) << 1) |
+				((0x80 & b[2]) >> 7);
+
+			unsigned long long fraction = 0;
+			fraction =
+				b[0] |
+				(b[1] << 8) |
+				((b[2] & 0x7f) << 16);
+
+			float x = 0;
+			for (std::size_t i = 0; i < 23; ++i)
+			{
+				if (fraction & (1 << i))
+					x += 1.0f;
+				x /= 2.0f;
+			}
+
+			if (exp == 0)
+			{
+				if (fraction == 0.0f)
+				{
+					f = sign * 0.0f;
+				}
+				else
+				{
+					f = sign * std::ldexp(x, -126);
+				}
+			}
+			else if (exp == 0xff)
+			{
+				if (fraction == 0.0f)
+				{
+					f = sign * INFINITY;
+				}
+				else
+				{
+					f = std::nanf("");
+				}
+			}
+			else
+			{
+				f = sign * std::ldexp(1 + x, exp - 127);
+			}
+
+			*pf = f;
+			return {};
+		}
+	};
+
+	template <>
+	struct encoding<float>
+	{
+		const float &my_f;
+
+		cause encode(tree_writer &w)
 		{
 			// Naive implementation... optimize by compiler later
+			float f = my_f;
+
 			auto type = std::fpclassify(f);
 
 			std::uint8_t b[4] = {0,0,0,0};
@@ -97,71 +170,8 @@ namespace gulachek::gtree
 				return {"Unknown float type"};
 			}
 
-			block blk{b, 4};
-			tree.value(blk);
-			return {};
-		}
-
-		template <typename Tree>
-		static error decode(Tree &&t, float &f)
-		{
-			// Naive implementation... optimize by compiler later
-			auto value = t.value();
-			if (value.size() != 4)
-			{
-				error err;
-				err << "Expected float to have 4 bytes. Tree had " << value.size();
-				return err;
-			}
-
-			auto b = value.data();
-
-			float sign = (0x80 & b[3]) ? -1.0f : 1.0f;
-			int exp =
-				((0x7f & b[3]) << 1) |
-				((0x80 & b[2]) >> 7);
-
-			unsigned long long fraction = 0;
-			fraction =
-				b[0] |
-				(b[1] << 8) |
-				((b[2] & 0x7f) << 16);
-
-			float x = 0;
-			for (std::size_t i = 0; i < 23; ++i)
-			{
-				if (fraction & (1 << i))
-					x += 1.0f;
-				x /= 2.0f;
-			}
-
-			if (exp == 0)
-			{
-				if (fraction == 0.0f)
-				{
-					f = sign * 0.0f;
-				}
-				else
-				{
-					f = sign * std::ldexp(x, -126);
-				}
-			}
-			else if (exp == 0xff)
-			{
-				if (fraction == 0.0f)
-				{
-					f = sign * INFINITY;
-				}
-				else
-				{
-					f = std::nanf("");
-				}
-			}
-			else
-			{
-				f = sign * std::ldexp(1 + x, exp - 127);
-			}
-
+			w.value(b, 4);
+			w.child_count(0);
 			return {};
 		}
 	};
---
+++ test/float_test.cpp
@@ -0,0 +1,397 @@
+#define BOOST_TEST_MODULE FloatTest
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include "gulachek/gtree/read.hpp"
+#include "gulachek/gtree/write.hpp"
+#include "gulachek/gtree/tree.hpp"
+#include "gulachek/gtree/encoding/float.hpp"
+#include <vector>
+#include <sstream>
+#include <cstdint>
+#include <cmath>
+
+using cause = gulachek::cause;
+namespace gt = gulachek::gtree;
+
+using vec = std::vector<std::uint8_t>;
+
+BOOST_AUTO_TEST_CASE(DecodeZero)
+{
+	vec val = { 0, 0, 0, 0 };
+	gt::tree tr{ val };
+	float f = 1;
+	std::stringstream ss;
+
+	gt::write(ss, tr);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &f);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(f == 0.0f);
+	BOOST_TEST(!std::signbit(f));
+}
+
+BOOST_AUTO_TEST_CASE(DecodeNegativeZero)
+{
+	vec val = { 0, 0, 0, 0x80 };
+	gt::tree tr{ val };
+	float f = 1;
+	std::stringstream ss;
+
+	gt::write(ss, tr);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &f);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(f == 0.0f);
+	BOOST_TEST(std::signbit(f));
+}
+
+BOOST_AUTO_TEST_CASE(DecodeEmptyIsError)
+{
+	gt::tree tr;
+	float f = 1;
+	std::stringstream ss;
+
+	gt::write(ss, tr);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &f);
+
+	BOOST_TEST(!!err);
+}
+
+BOOST_AUTO_TEST_CASE(DecodeOne)
+{
+	vec val = { 0, 0, 0x80, 0x3f };
+	gt::tree tr{ val };
+	float f = 0;
+	std::stringstream ss;
+
+	gt::write(ss, tr);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &f);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(f == 1.0f);
+}
+
+BOOST_AUTO_TEST_CASE(DecodeNegativeOne)
+{
+	vec val = { 0, 0, 0x80, 0xbf };
+	gt::tree tr{ val };
+	float f = 0;
+	std::stringstream ss;
+
+	gt::write(ss, tr);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &f);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(f == -1.0f);
+}
+
+BOOST_AUTO_TEST_CASE(DecodeSubnormal)
+{
+	vec val = { 0, 0, 0x40, 0x00 };
+	gt::tree tr{ val };
+	float f = 0;
+	std::stringstream ss;
+
+	gt::write(ss, tr);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &f);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(f == 5.87747175411e-39f);
+}
+
+BOOST_AUTO_TEST_CASE(DecodeNegativeSubnormal)
+{
+	vec val = { 0, 0, 0x40, 0x80 };
+	gt::tree tr{ val };
+	float f = 0;
+	std::stringstream ss;
+
+	gt::write(ss, tr);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &f);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(f == -5.87747175411e-39f);
+}
+
+BOOST_AUTO_TEST_CASE(DecodeInf)
+{
+	vec val = { 0, 0, 0x80, 0x7f };
+	gt::tree tr{ val };
+	float f = 0;
+	std::stringstream ss;
+
+	gt::write(ss, tr);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &f);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(std::isinf(f));
+	BOOST_TEST(f > 0);
+}
+
+BOOST_AUTO_TEST_CASE(DecodeNegativeInf)
+{
+	vec val = { 0, 0, 0x80, 0xff };
+	gt::tree tr{ val };
+	float f = 0;
+	std::stringstream ss;
+
+	gt::write(ss, tr);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &f);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(std::isinf(f));
+	BOOST_TEST(f < 0);
+}
+
+BOOST_AUTO_TEST_CASE(DecodeNan)
+{
+	vec val = { 0, 0, 0xc0, 0x7f };
+	gt::tree tr{ val };
+	float f = 0;
+	std::stringstream ss;
+
+	gt::write(ss, tr);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &f);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(std::isnan(f));
+}
+
+BOOST_AUTO_TEST_CASE(DecodeNormal)
+{
+	vec val = { 0x11, 0x11, 0x80, 0x3f };
+	gt::tree tr{ val };
+	float f = 0;
+	std::stringstream ss;
+
+	gt::write(ss, tr);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &f);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(f == 1.00052082539f);
+}
+
+BOOST_AUTO_TEST_CASE(DecodeNegativeNormal)
+{
+	vec val = { 0x11, 0x11, 0x80, 0xbf };
+	gt::tree tr{ val };
+	float f = 0;
+	std::stringstream ss;
+
+	gt::write(ss, tr);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &f);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(f == -1.00052082539f);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeInf)
+{
+	float f = INFINITY;
+
+	gt::tree tr;
+	std::stringstream ss;
+
+	gt::write(ss, f);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &tr);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(tr.child_count() == 0);
+
+	auto val = tr.value();
+	BOOST_TEST(val.size() == 4);
+	BOOST_TEST(val[0] == 0x00);
+	BOOST_TEST(val[1] == 0x00);
+	BOOST_TEST(val[2] == 0x80);
+	BOOST_TEST(val[3] == 0x7f);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeNegativeInf)
+{
+	float f = -INFINITY;
+
+	gt::tree tr;
+	std::stringstream ss;
+
+	gt::write(ss, f);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &tr);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(tr.child_count() == 0);
+
+	auto val = tr.value();
+	BOOST_TEST(val.size() == 4);
+	BOOST_TEST(val[0] == 0x00);
+	BOOST_TEST(val[1] == 0x00);
+	BOOST_TEST(val[2] == 0x80);
+	BOOST_TEST(val[3] == 0xff);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeNan)
+{
+	float f = std::nan("");
+
+	gt::tree tr;
+	std::stringstream ss;
+
+	gt::write(ss, f);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &tr);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(tr.child_count() == 0);
+
+	auto val = tr.value();
+	BOOST_TEST(val.size() == 4);
+	BOOST_TEST(val[0] == 0x01);
+	BOOST_TEST(val[1] == 0x00);
+	BOOST_TEST(val[2] == 0x80);
+	BOOST_TEST(val[3] == 0x7f);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeNormal)
+{
+	float f = 1.00052082539f;
+
+	gt::tree tr;
+	std::stringstream ss;
+
+	gt::write(ss, f);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &tr);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(tr.child_count() == 0);
+
+	auto val = tr.value();
+	BOOST_TEST(val.size() == 4);
+	BOOST_TEST(val[0] == 0x11);
+	BOOST_TEST(val[1] == 0x11);
+	BOOST_TEST(val[2] == 0x80);
+	BOOST_TEST(val[3] == 0x3f);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeNegativeNormal)
+{
+	float f = -1.00052082539f;
+
+	gt::tree tr;
+	std::stringstream ss;
+
+	gt::write(ss, f);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &tr);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(tr.child_count() == 0);
+
+	auto val = tr.value();
+	BOOST_TEST(val.size() == 4);
+	BOOST_TEST(val[0] == 0x11);
+	BOOST_TEST(val[1] == 0x11);
+	BOOST_TEST(val[2] == 0x80);
+	BOOST_TEST(val[3] == 0xbf);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeSubnormal)
+{
+	float f = 5.87747175411e-39f;
+
+	gt::tree tr;
+	std::stringstream ss;
+
+	gt::write(ss, f);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &tr);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(tr.child_count() == 0);
+
+	auto val = tr.value();
+	BOOST_TEST(val.size() == 4);
+	BOOST_TEST(val[0] == 0x00);
+	BOOST_TEST(val[1] == 0x00);
+	BOOST_TEST(val[2] == 0x40);
+	BOOST_TEST(val[3] == 0x00);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeNegativeSubnormal)
+{
+	float f = -5.87747175411e-39f;
+
+	gt::tree tr;
+	std::stringstream ss;
+
+	gt::write(ss, f);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &tr);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(tr.child_count() == 0);
+
+	auto val = tr.value();
+	BOOST_TEST(val.size() == 4);
+	BOOST_TEST(val[0] == 0x00);
+	BOOST_TEST(val[1] == 0x00);
+	BOOST_TEST(val[2] == 0x40);
+	BOOST_TEST(val[3] == 0x80);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeZero)
+{
+	float f = 0.0f;
+
+	gt::tree tr;
+	std::stringstream ss;
+
+	gt::write(ss, f);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &tr);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(tr.child_count() == 0);
+
+	auto val = tr.value();
+	BOOST_TEST(val.size() == 4);
+	BOOST_TEST(val[0] == 0x00);
+	BOOST_TEST(val[1] == 0x00);
+	BOOST_TEST(val[2] == 0x00);
+	BOOST_TEST(val[3] == 0x00);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeNegativeZero)
+{
+	float f = -0.0f;
+
+	gt::tree tr;
+	std::stringstream ss;
+
+	gt::write(ss, f);
+	ss.seekg(0, std::ios::beg);
+	auto err = gt::read(ss, &tr);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(tr.child_count() == 0);
+
+	auto val = tr.value();
+	BOOST_TEST(val.size() == 4);
+	BOOST_TEST(val[0] == 0x00);
+	BOOST_TEST(val[1] == 0x00);
+	BOOST_TEST(val[2] == 0x00);
+	BOOST_TEST(val[3] == 0x80);
+}
