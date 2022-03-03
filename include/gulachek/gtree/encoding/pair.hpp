83fd0318ebf1eec26ec14f3a2c19ad28c301f4e2
Nicholas Gulachek <gulac004@umn.edu>
2022-03-03 09:36:50 -0600

    add pair support

--- include/gulachek/gtree/encoding/pair.hpp
+++ include/gulachek/gtree/encoding/pair.hpp
@@ -1,10 +1,75 @@
 #ifndef GULACHEK_GTREE_ENCODING_PAIR_HPP
 #define GULACHEK_GTREE_ENCODING_PAIR_HPP
 
-#include "gulachek/gtree/encoding/encoding.hpp"
+#include "gulachek/gtree/encoding.hpp"
+#include "gulachek/gtree/decoding.hpp"
+
+#include <utility>
 
 namespace gulachek::gtree
 {
+	template <typename T, typename U>
+	struct decoding<std::pair<T, U>>
+	{
+		std::pair<T, U> *p;
+
+		cause decode(treeder &r)
+		{
+			if (r.child_count() < 2)
+			{
+				cause err;
+				err << "not enough children to decode pair. child_count: " <<
+					r.child_count();
+				return err;
+			}
+
+			if (auto err = r.read(&p->first))
+			{
+				cause wrap{"error reading first pair elem"};
+				wrap.add_cause(err);
+				return wrap;
+			}
+
+			if (auto err = r.read(&p->second))
+			{
+				cause wrap{"error reading second pair elem"};
+				wrap.add_cause(err);
+				return wrap;
+			}
+
+			return {};
+		}
+	};
+
+	template <typename T, typename U>
+	struct encoding<std::pair<T, U>>
+	{
+		const std::pair<T, U> &p;
+
+		cause encode(tree_writer &w)
+		{
+			w.value(nullptr, 0);
+			w.child_count(2);
+			
+			if (auto err = w.write(p.first))
+			{
+				cause wrap{"error writing first pair elem"};
+				wrap.add_cause(err);
+				return wrap;
+			}
+
+			if (auto err = w.write(p.second))
+			{
+				cause wrap{"error writing second pair elem"};
+				wrap.add_cause(err);
+				return wrap;
+			}
+
+			return {};
+		}
+	};
+
+	/*
 	template <typename K, typename V>
 	struct encoding<std::pair<K,V>>
 	{
@@ -43,6 +108,7 @@ namespace gulachek::gtree
 					tree.child(1));
 		}
 	};
+	*/
 }
 
 #endif
---
+++ test/pair_test.cpp
@@ -0,0 +1,123 @@
+#define BOOST_TEST_MODULE PairTest
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include "convert.hpp"
+#include "saboteur.hpp"
+
+#include "gulachek/gtree/tree.hpp"
+#include "gulachek/gtree/encoding/pair.hpp"
+#include "gulachek/gtree/encoding/unsigned.hpp"
+#include "gulachek/gtree/encoding/string.hpp"
+
+#include <vector>
+#include <sstream>
+#include <cstdint>
+
+namespace tt = boost::test_tools;
+
+using cause = gulachek::cause;
+namespace gt = gulachek::gtree;
+
+using vec = std::vector<std::uint8_t>;
+
+BOOST_AUTO_TEST_CASE(DecodeStrint)
+{
+	gt::tree tr;
+	std::pair<std::string, unsigned int> pair;
+
+	std::string hello = "hello";
+	unsigned int n = 3;
+
+	tr.child_count(2);
+	cvt(hello, &tr.child(0));
+	cvt(n, &tr.child(1));
+
+	auto err = cvt(tr, &pair);
+
+	BOOST_TEST(!err);
+
+	BOOST_TEST(pair.first == "hello");
+	BOOST_TEST(pair.second == 3);
+}
+
+BOOST_AUTO_TEST_CASE(NotEnoughChildrenIsError)
+{
+	gt::tree tr;
+	std::pair<std::string, unsigned int> pair;
+
+	auto err = cvt(tr, &pair);
+
+	BOOST_TEST(!!err);
+}
+
+BOOST_AUTO_TEST_CASE(DecodeErrorInFirstElemIsError)
+{
+	gt::tree tr;
+	std::pair<saboteur, std::string> pair;
+
+	tr.child_count(2);
+	std::uint8_t n = 1;
+	cvt(n, &tr.child(0));
+
+	auto err = cvt(tr, &pair);
+
+	BOOST_TEST(!!err);
+}
+
+BOOST_AUTO_TEST_CASE(DecodeErrorInSecondElemIsError)
+{
+	gt::tree tr;
+	std::pair<std::string, saboteur> pair;
+
+	tr.child_count(2);
+	std::uint8_t n = 1;
+	cvt(n, &tr.child(1));
+
+	auto err = cvt(tr, &pair);
+
+	BOOST_TEST(!!err);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeStrint)
+{
+	std::pair<std::string, unsigned int> pair{"hello", 3};
+
+	gt::tree tr;
+
+	auto err = cvt(pair, &tr);
+
+	BOOST_REQUIRE(!err);
+
+	std::string s;
+	unsigned int n = 0;
+
+	BOOST_TEST(tr.value().empty());
+	BOOST_TEST(tr.child_count() == 2);
+	
+	cvt(tr.child(0), &s);
+	cvt(tr.child(1), &n);
+
+	BOOST_TEST(s == "hello");
+	BOOST_TEST(n == 3);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeErrorInFirstElemIsError)
+{
+	std::pair<saboteur, std::string> pair{{true}, "hello"};
+
+	gt::tree tr;
+	auto err = cvt(pair, &tr);
+
+	BOOST_TEST(!!err);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeErrorInSecondElemIsError)
+{
+	std::pair<std::string, saboteur> pair{"hello", {true}};
+
+	gt::tree tr;
+	auto err = cvt(pair, &tr);
+
+	BOOST_TEST(!!err);
+}
