514c76c06b3b962da04ce217280d63b3d4131023
Nicholas Gulachek <gulac004@umn.edu>
2022-03-03 10:49:23 -0600

    shared_ptr support

---
+++ include/gulachek/gtree/encoding/shared_ptr.hpp
@@ -0,0 +1,44 @@
+#ifndef GULACHEK_GTREE_ENCODING_SHARED_PTR_HPP
+#define GULACHEK_GTREE_ENCODING_SHARED_PTR_HPP
+
+#include "gulachek/gtree/encoding.hpp"
+#include "gulachek/gtree/decoding.hpp"
+
+#include <memory>
+
+namespace gulachek::gtree
+{
+	template <typename T>
+	struct decoding<std::shared_ptr<T>>
+	{
+		std::shared_ptr<T> *pp;
+
+		cause decode(treeder &r)
+		{
+			T val;
+			decoding<T> dec{&val};
+			if (auto err = dec.decode(r))
+				return err;
+
+			*pp = std::make_shared<T>(std::move(val));
+			return {};
+		}
+	};
+
+	template <typename T>
+	struct encoding<std::shared_ptr<T>>
+	{
+		const std::shared_ptr<T> &p;
+
+		cause encode(tree_writer &w)
+		{
+			if (!p)
+				return {"cannot encode nullptr for std::shared_ptr"};
+
+			encoding<T> enc{*p};
+			return enc.encode(w);
+		}
+	};
+}
+
+#endif
---
+++ test/shared_pointer_test.cpp
@@ -0,0 +1,70 @@
+#define BOOST_TEST_MODULE SharedPtrTest
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include "convert.hpp"
+#include "saboteur.hpp"
+
+#include "gulachek/gtree/encoding/signed.hpp"
+#include "gulachek/gtree/encoding/shared_ptr.hpp"
+
+#include <vector>
+#include <sstream>
+#include <cstdint>
+
+namespace tt = boost::test_tools;
+
+using cause = gulachek::cause;
+namespace gt = gulachek::gtree;
+
+BOOST_AUTO_TEST_CASE(DecodeValueAsPointer)
+{
+	int n = 3;
+	std::shared_ptr<int> p;
+
+	auto err = cvt(n, &p);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(*p == 3);
+}
+
+BOOST_AUTO_TEST_CASE(DecodeErrorInValueIsError)
+{
+	int n = 3;
+	std::shared_ptr<saboteur> p;
+
+	auto err = cvt(n, &p);
+
+	BOOST_TEST(!!err);
+}
+
+BOOST_AUTO_TEST_CASE(EncodePointerAsValue)
+{
+	std::shared_ptr<int> p = std::make_shared<int>(3);
+	int n = 0;
+
+	auto err = cvt(p, &n);
+
+	BOOST_TEST(!err);
+	BOOST_TEST(n == 3);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeNullIsError)
+{
+	std::shared_ptr<int> p;
+	std::stringstream ss;
+
+	auto err = gt::write(ss, p);
+
+	BOOST_TEST(!!err);
+}
+
+BOOST_AUTO_TEST_CASE(EncodeErrorInValIsError)
+{
+	auto p = std::make_shared<saboteur>(saboteur{true});
+	std::stringstream ss;
+
+	auto err = gt::write(ss, p);
+
+	BOOST_TEST(!!err);
+}
