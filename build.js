be1273737e310408d77a71d823aadc3cd9e4e0b8
Nicholas Gulachek <gulac004@umn.edu>
2022-09-05 20:20:00 -0500

    Update build system to use commander

--- build.js
+++ build.js
@@ -5,115 +5,167 @@ const { CppSystem } = require('gulpachek/cpp');
 const fs = require('fs');
 const { spawn } = require('child_process');
 const { version } = require('./package.json');
-
-const [node, script, target] = process.argv;
-
-if (!target) {
-	console.error('Must specify <target> as first positional command line param');
-	process.exit(1);
-}
+const { Command } = require('commander');
 
 if (!version) {
 	console.error(new Error('gtree package.json version not specified'));
 	process.exit(1);
 }
 
-const sys = new BuildSystem(__dirname);
-const cpp = new CppSystem({
-	sys,
-	cppVersion: 20
-});
-
-const boost = {
-	test: cpp.require('org.boost.test', '1.74.0', 'dynamic'),
-	fiber: cpp.require('org.boost.fiber', '1.74.0', 'dynamic'),
-	iostreams: cpp.require('org.boost.iostreams', '1.74.0', 'dynamic')
-};
-
-const gtree = cpp.compile({
-	name: 'com.gulachek.gtree',
-	version,
-	apiDef: 'GTREE_API',
-	src: [
-		'src/base128.cpp',
-		'src/twos_complement.cpp',
-		'src/fd.cpp',
-		'src/cause.cpp',
-		'src/translate.cpp',
-		'src/tree.cpp'
-	]
-});
-
-gtree.include('include');
-gtree.link(boost.fiber);
-gtree.link(boost.iostreams);
-
-const image = gtree.image();
-
-const testBuildRules = [];
-const tests = [];
-
-// not ideal that this is using relative path. should be relative to srcdir
-for (const f of fs.readdirSync('test')) {
-	if (!f.match(/_test.cpp$/)) { continue; }
-	const name = f.substring(0, f.length - '_test.cpp'.length);
-
-	const test = cpp.compile({
-		name: `${name}_test`,
-		src: [`test/${f}`]
+function configure() {
+	const sys = new BuildSystem();
+	const cpp = new CppSystem({
+		sys,
+		cppVersion: 20
 	});
 
-	test.link(image);
-	test.link(boost.test);
-	test.include('test/include');
+	return { sys, cpp };
+}
 
-	const target = test.executable();
-	testBuildRules.push(target);
+function makeLib(opts) {
+	const { cpp } = opts;
 
-	const runTest = () => {
-		return spawn(target.abs(), [], { stdio: 'inherit' });
+	const boost = {
+		fiber: cpp.require('org.boost.fiber', '1.74.0', 'dynamic'),
+		iostreams: cpp.require('org.boost.iostreams', '1.74.0', 'dynamic')
 	};
 
-	Object.defineProperty(runTest, 'name', {
-		value: target.path().toString(),
-		writable: false
+	const gtree = cpp.compile({
+		name: 'com.gulachek.gtree',
+		version,
+		apiDef: 'GTREE_API',
+		src: [
+			'src/base128.cpp',
+			'src/twos_complement.cpp',
+			'src/fd.cpp',
+			'src/cause.cpp',
+			'src/translate.cpp',
+			'src/tree.cpp'
+		]
 	});
 
-	tests.push(runTest);
+	gtree.include('include');
+	gtree.link(boost.fiber);
+	gtree.link(boost.iostreams);
+
+	return gtree;
 }
 
-const gtree2hex = cpp.compile({
-	name: 'gtree2hex',
-	version,
-	src: ['tools/gtree2hex.cpp']
+const program = new Command();
+
+program
+  .name('build.js')
+  .description('Build gtree libraries')
+  .option('--build-type <type>', 'debug or release')
+  .version(version);
+
+program.command('build')
+.description('Build the library')
+.action((_, options) => {
+
+	const { cpp, sys } = configure();
+
+	const gtree = makeLib({ cpp });
+
+	sys.build(gtree.image().binary());
+
 });
 
-gtree2hex.link(image);
-testBuildRules.push(gtree2hex.executable());
+program.command('test')
+.description('Build and run unit tests')
+.action((options) => {
+	const { cpp, sys } = configure();
 
-const nums = cpp.compile({
-	name: 'nums',
-	src: ['example/nums.cpp']
-});
+	const gtree = makeLib({ cpp });
 
-nums.link(image);
-testBuildRules.push(nums.executable());
+	const boost = {
+		test: cpp.require('org.boost.test', '1.74.0', 'dynamic')
+	};
 
-const testRule = parallel(...testBuildRules.map(rule => sys.rule(rule)));
+	const image = gtree.image();
 
-const asyncCb = (err) => {
-	if (err) { console.error(err); }
-}
+	const testBuildRules = [];
+	const tests = [];
 
-const buildRule = sys.rule(image.binary());
+	// not ideal that this is using relative path. should be relative to srcdir
+	for (const f of fs.readdirSync('test')) {
+		if (!f.match(/_test.cpp$/)) { continue; }
+		const name = f.substring(0, f.length - '_test.cpp'.length);
 
-if (target === 'build') {
-	asyncDone(buildRule, asyncCb);
-} else if (target === 'test') {
+		const test = cpp.compile({
+			name: `${name}_test`,
+			src: [`test/${f}`]
+		});
+
+		test.link(image);
+		test.link(boost.test);
+		test.include('test/include');
+
+		const target = test.executable();
+		testBuildRules.push(target);
+
+		const runTest = () => {
+			return spawn(target.abs(), [], { stdio: 'inherit' });
+		};
+
+		Object.defineProperty(runTest, 'name', {
+			value: target.path().toString(),
+			writable: false
+		});
+
+		tests.push(runTest);
+	}
+
+	const testRule = parallel(...testBuildRules.map(rule => sys.rule(rule)));
+
+	const asyncCb = (err) => {
+		if (err) { console.error(err); }
+	}
+
+	const buildRule = sys.rule(image.binary());
+
 	asyncDone(series(buildRule, testRule, ...tests), asyncCb);
-} else if (target === 'pack') {
-	asyncDone(sys.rule(cpp.pack(image)), asyncCb);
-} else {
-	console.error(`Unknown target: ${target}`);
-	process.exit(1);
-}
+});
+
+program.command('pack')
+.description('Package build outputs for installers')
+.action(async (options) => {
+	for (const opts of [
+		{ buildType: 'debug' },
+		{ buildType: 'release' }
+	]) {
+		process.env.GULPACHEK_BUILD_TYPE = opts.buildType;
+		const { cpp, sys } = configure(opts);
+
+		const gtree = makeLib({ cpp });
+
+		await sys.build(cpp.pack(gtree.image()));
+	}
+});
+
+program.command('test-install')
+.description('Build programs against installed library')
+.action((options) => {
+	const { cpp, sys } = configure();
+
+	const gtree = cpp.require('com.gulachek.gtree', version, 'dynamic');
+
+	const gtree2hex = cpp.compile({
+		name: 'gtree2hex',
+		version,
+		src: ['tools/gtree2hex.cpp']
+	});
+
+	gtree2hex.link(gtree);
+	sys.build(gtree2hex.executable());
+
+	const nums = cpp.compile({
+		name: 'nums',
+		src: ['example/nums.cpp']
+	});
+
+	nums.link(gtree);
+	sys.build(nums.executable());
+});
+
+program.parse();
